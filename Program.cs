8f0a3bb5e0c8cbfa7e457a3de9165a7e89de16bd
erika20087 <oku_e@bryanston.co.uk>
2026-06-11 21:09:06 +0000

    fhf

--- Program.cs
+++ Program.cs
@@ -6,6 +6,26 @@ class Program
     {
         // Here is your empty program!
         Console.Clear();
-        Console.WriteLine("Hello world!");
+        Console.WriteLine("Enter the message: ");
+        string message = Console.Readline();
+        Condole.WriteLine("Enter limit: ");
+        int limit = Convert.ToInt32(Console.ReadLine());
+
+        int index = 0;
+        int total = 0;
+
+        foreach(char c in message)
+        {
+            int i = int(c);
+            total += i;
+        }
+        
+        int total2 = total * total;
+        string totalString = total2.ToString();
+        int middleIndex = totalString.Length / 2;
+        string middle2 = totalString.Substring(middleIndex, 2);
+        int middle = Convert.ToInt32(middle2);
+        int hash = middle%limit;
+        Console.WriteLine("Hash value is: ", hash);
     }
 }
